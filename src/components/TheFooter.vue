0746acb46ade589cab80c3f6156915494af922f7
SerhiiKarnaukh <serhiikarnaukh.cn@gmail.com>
2023-11-11 23:16:54 +0200

    Add: Links

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,17 +1,26 @@
 <template>
   <v-footer color="primary">
-    <v-row
-      justify="center"
-      no-gutters
-    >
+    <v-row justify="center" no-gutters>
+      <v-btn color="white" variant="text" class="mx-2" rounded="xl" to="/">
+        Home
+      </v-btn>
       <v-btn
         color="white"
         variant="text"
         class="mx-2"
         rounded="xl"
-        to="/"
+        :href="remotedDjangoHost"
       >
-        Home
+        All Apps
+      </v-btn>
+      <v-btn
+        color="white"
+        variant="text"
+        class="mx-2"
+        rounded="xl"
+        :href="remotedVueHost"
+      >
+        Vue Apps
       </v-btn>
       <div v-if="!$store.state.auth.token">
         <v-btn
@@ -26,10 +35,7 @@
           {{ link.label }}
         </v-btn>
       </div>
-      <v-col
-        class="text-center mt-4"
-        cols="12"
-      >
+      <v-col class="text-center mt-4" cols="12">
         {{ new Date().getFullYear() }} — <strong>Games Store</strong>
       </v-col>
     </v-row>
@@ -37,21 +43,22 @@
 </template>
 
 <script>
-  export default {
-    data: () => ({
-      links: [
-        {
-          label: 'Signup',
-          url: '/signup'
-        },
-        {
-          label: 'Login',
-          url: '/login'
-        },
-      ],
-    }),
-  }
+export default {
+  data: () => ({
+    links: [
+      {
+        label: 'Signup',
+        url: '/signup',
+      },
+      {
+        label: 'Login',
+        url: '/login',
+      },
+    ],
+    remotedDjangoHost: import.meta.env.VITE_REMOTE_DJANGO_HOST,
+    remotedVueHost: import.meta.env.VITE_REMOTE_VUE_HOST,
+  }),
+}
 </script>
 
-<style lang="scss" scoped>
-</style>
+<style lang="scss" scoped></style>
--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -6,16 +6,32 @@
         @click.stop="drawer = !drawer"
         class="d-sm-flex d-md-none"
       ></v-app-bar-nav-icon>
-      <router-link
-        to="/"
-        style="text-decoration:none;"
-      >
+      <router-link to="/" style="text-decoration: none">
         <v-app-bar-title>
           <h3 class="main_title ml-3">Games Store</h3>
         </v-app-bar-title>
       </router-link>
       <v-spacer></v-spacer>
       <div class="d-md-flex d-sm-none d-none mr-3">
+        <v-menu open-on-hover>
+          <template v-slot:activator="{ props }">
+            <v-btn
+              color="white"
+              v-bind="props"
+              prepend-icon="mdi-remote-desktop"
+            >
+              Apps Manager
+            </v-btn>
+          </template>
+          <v-list>
+            <v-list-item :href="remotedDjangoHost"
+              ><v-list-item-title>All Apps</v-list-item-title></v-list-item
+            >
+            <v-list-item :href="remotedVueHost"
+              ><v-list-item-title>Vue Apps</v-list-item-title></v-list-item
+            >
+          </v-list>
+        </v-menu>
         <div v-if="!$store.state.auth.token">
           <v-btn
             v-for="link in links"
@@ -28,15 +44,9 @@
           >
             {{ link.label }}
           </v-btn>
-
         </div>
         <div v-if="$store.state.auth.token">
-          <v-btn
-            flat
-            color="white"
-            prepend-icon="mdi-logout"
-            @click="logout"
-          >
+          <v-btn flat color="white" prepend-icon="mdi-logout" @click="logout">
             Logout
           </v-btn>
         </div>
@@ -51,12 +61,28 @@
         </v-btn>
       </div>
     </v-app-bar>
-    <v-navigation-drawer
-      temporary
-      v-model="drawer"
-      location="left"
-    >
+    <v-navigation-drawer temporary v-model="drawer" location="left">
       <v-list>
+        <v-menu>
+          <template v-slot:activator="{ props }">
+            <v-list-item
+              color="white"
+              v-bind="props"
+              prepend-icon="mdi-remote-desktop"
+            >
+              Apps Manager
+            </v-list-item>
+          </template>
+
+          <v-list>
+            <v-list-item :href="remotedDjangoHost"
+              ><v-list-item-title>All Apps</v-list-item-title></v-list-item
+            >
+            <v-list-item :href="remotedVueHost"
+              ><v-list-item-title>Vue Apps</v-list-item-title></v-list-item
+            >
+          </v-list>
+        </v-menu>
         <div v-if="!$store.state.auth.token">
           <v-list-item
             v-for="item in links"
@@ -92,44 +118,49 @@
 
 <script>
 import { useTheme } from 'vuetify'
-import {useRouter} from 'vue-router'
-import {useStore} from 'vuex'
-  export default {
-    setup () {
+import { useRouter } from 'vue-router'
+import { useStore } from 'vuex'
+export default {
+  setup() {
     const theme = useTheme()
     const router = useRouter()
     const store = useStore()
 
     return {
       theme,
-      toggleTheme: () => theme.global.name.value = theme.global.current.value.dark ? 'CustomLightTheme' : 'dark',
+      toggleTheme: () =>
+        (theme.global.name.value = theme.global.current.value.dark
+          ? 'CustomLightTheme'
+          : 'dark'),
       logout: () => {
         store.commit('auth/logout')
         router.push('/login')
       },
     }
-   },
-    data: () => ({
-      drawer: false,
-      links: [
-        {
-          icon: 'login',
-          label: 'Login',
-          url: '/login'
-        },
-        {
-          icon: 'pencil-lock',
-          label: 'Signup',
-          url: '/signup'
-        },
-      ],
-    }),
-    watch: {
+  },
+  data: () => ({
+    drawer: false,
+    links: [
+      {
+        icon: 'login',
+        label: 'Login',
+        url: '/login',
+      },
+      {
+        icon: 'pencil-lock',
+        label: 'Signup',
+        url: '/signup',
+      },
+    ],
+    remotedDjangoHost: import.meta.env.VITE_REMOTE_DJANGO_HOST,
+    remotedVueHost: import.meta.env.VITE_REMOTE_VUE_HOST,
+  }),
+  watch: {
     $route() {
       this.drawer = false
     },
   },
-  }
+}
 </script>
 
 <style scoped>
